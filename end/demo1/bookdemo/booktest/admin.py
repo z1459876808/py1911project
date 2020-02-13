7d710257c42a3dff12b5bbda885e24cc6732eb7c
zcx001 <1459876808@qq.com>
2020-02-13 11:38:43 +0800

    站点管理 及 英雄图书关联

--- end/demo1/bookdemo/booktest/admin.py
+++ end/demo1/bookdemo/booktest/admin.py
@@ -1,9 +1,34 @@
 from django.contrib import admin
 
+from django.contrib.admin import ModelAdmin
+
 # Register your models here.
 
 
 # 注册自己需要管理的模型 Book Hero
-from .models import Book,Hero
-admin.site.register(Book)
-admin.site.register(Hero)
+from .models import Book, Hero
+
+
+class HeroInline(admin.StackedInline):
+    model = Hero
+    extra = 1
+
+
+class HeroAdmin(ModelAdmin):
+    list_display = ('name', 'gender', 'content', 'book')
+
+    search_fields = ('name', 'gender')
+
+
+admin.site.register(Hero, HeroAdmin)
+
+
+class BookAdmin(ModelAdmin):
+    list_display = ('title', 'price', 'pub_date')
+
+    search_fields = ('title', 'price')
+
+    inlines = [HeroInline]
+
+
+admin.site.register(Book, BookAdmin)
