5f647c1b3ed9db271c17daf0844d8ea64006df1e
zcx001 <1459876808@qq.com>
2020-02-14 19:36:45 +0800

    编辑与添加

--- end/demo1/bookdemo/booktest/urls.py
+++ end/demo1/bookdemo/booktest/urls.py
@@ -2,9 +2,17 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'booktest'
 # 每一个路由文件中必须编写一个路由数组
 urlpatterns = [
-    url(r'^index/$', views.index),
-    url(r'^about/$', views.about),
-    url(r'^detail/(\d+)', views.detail)
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^detail/(\d+)', views.detail, name='detail'),
+
+    url(r'^deletebook/(\d+)/$', views.deletebook, name='deletebook'),
+    url(r'^deletehero/(\d+)/$', views.deletehero, name='deletehero'),
+    url(r'^addbook/$', views.addbook, name='addbook'),
+    url(r'^editbook/(\d+)/$',views.editbook,name='editbook'),
+    url(r'^addhero/(\d+)/$', views.addhero, name='addhero'),
+    url(r'^edithero/(\d+)/$', views.edithero, name='edithero')
 ]
--- end/demo1/bookdemo/booktest/views.py
+++ end/demo1/bookdemo/booktest/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.template import loader
 
 # Create your views here.
@@ -35,3 +35,70 @@ def detail(request, bookid):
     # return HttpResponse(resault)
 
     return render(request, 'detail.html', {'book': book})
+
+
+def deletebook(request, bookid):
+    book = Book.objects.get(id=bookid)
+    book.delete()
+    return redirect(to='/')
+
+
+def deletehero(request, heroid):
+    hero = Hero.objects.get(id=heroid)
+    bookid = hero.book.id
+    hero.delete()
+    url = reverse("booktest:detail", args=(bookid,))
+    return redirect(to=url)
+
+
+def addbook(requset):
+    if requset.method == 'GET':
+        return render(requset, 'addbook.html')
+    elif requset.method == 'POST':
+        book = Book()
+        book.title = requset.POST.get('booktitle')
+        book.pub_date = requset.POST.get('bookdate')
+        book.price = requset.POST.get('bookprice')
+        book.save()
+        url = reverse('booktest:index')
+        return redirect(to=url)
+
+
+def editbook(requset, bookid):
+    book = Book.objects.get(id=bookid)
+    if requset.method == 'GET':
+        return render(requset, 'editbook.html', {'book': book})
+    elif requset.method == 'POST':
+        book.title = requset.POST.get('booktitle')
+        book.pub_date = requset.POST.get('bookdate')
+        book.price = requset.POST.get('bookprice')
+        book.save()
+        return redirect(to='/')
+
+
+def addhero(request, bookid):
+    if request.method == 'GET':
+        return render(request, 'addhero.html')
+    elif request.method == 'POST':
+        hero = Hero()
+        hero.name = request.POST.get('heroname')
+        hero.content = request.POST.get('herocontent')
+        hero.gender = request.POST.get('sex')
+        hero.book = Book.objects.get(id=bookid)
+        hero.save()
+        url = reverse('booktest:detail', args=(bookid,))
+        return redirect(to=url)
+
+
+def edithero(request, heroid):
+    # 使用get方法进入英雄编辑页面
+    hero = Hero.objects.get(id=heroid)
+    if request.method == 'GET':
+        return render(request, 'edithero.html', {'hero': hero})
+    elif request.method == 'POST':
+        hero.name = request.POST.get('heroname')
+        hero.content = request.POST.get('herocontent')
+        hero.gender = request.POST.get('sex')
+        hero.save()
+        url = reverse('booktest:detail', args=(hero.book.id,))
+        return redirect(to=url)
