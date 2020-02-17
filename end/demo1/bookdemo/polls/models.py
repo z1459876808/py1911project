61f65367117cd481fff54cc227e7a02b91381b64
zcx001 <1459876808@qq.com>
2020-02-17 14:05:21 +0800

    投票小程序

--- end/demo1/bookdemo/polls/models.py
+++ end/demo1/bookdemo/polls/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Article(models.Model):
-    title=models.CharField(max_length=20,verbose_name="标题")
+    title = models.CharField(max_length=20, verbose_name="标题")
 
     def __str__(self):
         return self.title
 
 
 class Ticket(models.Model):
-    content = models.CharField(verbose_name="投票内容",max_length=20)
-    count = models.FloatField(verbose_name="投票数量",default=6)
-    contact=models.ForeignKey(Article,on_delete=models.CASCADE,related_name="articles")
+    content = models.CharField(verbose_name="投票内容", max_length=20)
+    count = models.FloatField(verbose_name="投票数量", default=6)
+    contact = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="articles")
+
     def __str__(self):
         return self.content
--- end/demo1/bookdemo/polls/urls.py
+++ end/demo1/bookdemo/polls/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from . import views
 
-app_name="polls"
+app_name = "polls"
 
 urlpatterns = [
 
-    url(r'^$', views.index,name='index'),
-    url(r'^detail/(\d+)/$',views.detail,name='detail'),
-    url(r'^result/(\d+)/$',views.result,name='result'),
+    url(r'^$', views.index, name='index'),
+    url(r'^detail/(\d+)/$', views.detail, name='detail'),
+    url(r'^result/(\d+)/$', views.result, name='result'),
 ]
